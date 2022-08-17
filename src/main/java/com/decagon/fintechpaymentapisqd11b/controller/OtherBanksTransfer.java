a6de7ec168da01575c0a7752e31031ad5145a699
monsaw <lawalmonsaw@gmail.com>
2022-08-17 22:53:09 +0100

    implemented verify transfer method..

--- src/main/java/com/decagon/fintechpaymentapisqd11b/controller/OtherBanksTransfer.java
+++ src/main/java/com/decagon/fintechpaymentapisqd11b/controller/OtherBanksTransfer.java
@@ -3,6 +3,7 @@ package com.decagon.fintechpaymentapisqd11b.controller;
 import com.decagon.fintechpaymentapisqd11b.entities.FlwBank;
 import com.decagon.fintechpaymentapisqd11b.request.VerifyTransferRequest;
 import com.decagon.fintechpaymentapisqd11b.service.TransferService;
+import com.decagon.fintechpaymentapisqd11b.service.serviceImpl.OtherBanksTransferImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +13,10 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping(path="/api/v1/transfer")
 public class OtherBanksTransfer {
-    private TransferService transferService;
+    private final TransferService transferService;
+
+    private final OtherBanksTransferImpl otherBanksTransfer;
+
 
     @GetMapping("/banks")
     public List<FlwBank> getBanks() {
@@ -22,6 +26,6 @@ public class OtherBanksTransfer {
 
     @PostMapping("/verify-transfer")
     public void verify(@RequestBody VerifyTransferRequest verifyTransferRequest){
-
+        otherBanksTransfer.verifyTransfer(verifyTransferRequest);
     }
 }
