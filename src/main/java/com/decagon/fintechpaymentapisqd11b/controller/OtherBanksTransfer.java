1f2c2de237c24e55a6e833384804c5f85637499e
monsaw <lawalmonsaw@gmail.com>
2022-08-17 22:55:37 +0100

    implemented verify transfer method..

--- src/main/java/com/decagon/fintechpaymentapisqd11b/controller/OtherBanksTransfer.java
+++ src/main/java/com/decagon/fintechpaymentapisqd11b/controller/OtherBanksTransfer.java
@@ -26,6 +26,7 @@ public class OtherBanksTransfer {
 
     @PostMapping("/verify-transfer")
     public void verify(@RequestBody VerifyTransferRequest verifyTransferRequest){
+
         otherBanksTransfer.verifyTransfer(verifyTransferRequest);
     }
 }
